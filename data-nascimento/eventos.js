2b89636837ee4ed6ea3b40205775a496e4564e1f
1pedrog <pedro.galeno.loiola@gmail.com>
2022-08-01 18:53:25 -0300

    Corrigi array

--- data-nascimento/eventos.js
+++ data-nascimento/eventos.js
@@ -6,10 +6,26 @@ for (let d = 1; d <= 31; d++) {
     SELECT_DIA.innerHTML += '<option>'+d+'</option>';
 }
 
-for (let m = 1; m <= 12; m++) {
-    SELECT_MES.innerHTML += '<option>'+m+'</option>';
+let meses = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+]
+
+for (let m = 0; m <= 11; m++) {
+    SELECT_MES.innerHTML += `<option>${meses[m]}</option>`;
 }
 
 for (let a = 1900; a <= 2022; a++) {
     SELECT_ANO.innerHTML += '<option>'+a+'</option>';
 }
+
