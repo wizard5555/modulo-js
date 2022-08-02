037a9bb167f2153e288817df84b37080918e938d
1pedrog <pedro.galeno.loiola@gmail.com>
2022-08-01 21:14:30 -0300

    Projeto inicial

--- endereco-js/buscar.js
+++ endereco-js/buscar.js
@@ -1,6 +1,31 @@
+let enderecos = {
+    60352100: {
+        logradouro: 'Av Santos Dumont',
+        bairro: 'Aldeota',
+        cidade: 'FortalCity',
+        uf: 'CE',
+    },
+
+    60352499: {
+        logradouro: 'Rua Vicente Leite',
+        bairro: 'Varjota',
+        cidade: 'FortalCity',
+        uf: 'CE',
+    },
+
+    60100100: {
+        logradouro: 'Av Bezerra de Menezes',
+        bairro: 'Parquelandia',
+        cidade: 'FortalCity',
+        uf: 'CE',
+    },
+}
+
 CEP.addEventListener('blur', function () {
-   LOGRADOURO.value = 'Rua X';
-   BAIRRO.value = 'Aldeota';
-   CIDADE.value = 'FortalCity';
-   UF.value = 'CE';
+    let valor = CEP.value;
+
+    LOGRADOURO.value = enderecos[valor].logradouro;
+    BAIRRO.value = enderecos[valor].bairro;
+    CIDADE.value = enderecos[valor].cidade;
+    UF.value = enderecos[valor].uf;
 })
---
+++ tabela/eventos.js
@@ -0,0 +1,60 @@
+let bebidas = [
+    {
+        nome: 'Heineken',
+        descricao: 'Longneck 330ml',
+        quantidade: 12
+    },
+    {
+        nome: 'Jack Daniels',
+        descricao: 'litro 1L',
+        quantidade: 6
+    },
+    {
+        nome: 'OldParr',
+        descricao: 'garrafa 750ml',
+        quantidade: 12
+    },
+    {
+        nome: 'Heineken',
+        descricao: 'garrafa 600ml',
+        quantidade: 13
+    },
+    {
+        nome: 'Campari',
+        descricao: 'garrafa 1L',
+        quantidade: 12
+    },
+    {
+        nome: 'Vinho São Braz',
+        descricao: 'garrafa 1L',
+        quantidade: 10
+    },
+    {
+        nome: 'Skol',
+        descricao: 'buchudinha 330ml',
+        quantidade: 12
+    },
+    {
+        nome: 'Budweiser',
+        descricao: 'Longneck 330ml',
+        quantidade: 36
+    },
+    {
+        nome: 'Budweiser',
+        descricao: 'garrafa 600ml',
+        quantidade: 36
+    },
+    {
+        nome: 'Eisenbahn',
+        descricao: 'Longneck 330ml',
+        quantidade: 24
+    },
+    {
+        nome: 'Eisenbahn',
+        descricao: 'garrafa 600ml',
+        quantidade: 24
+    },
+];
+
+const TABELA = document.getElementById('tabela');
+
