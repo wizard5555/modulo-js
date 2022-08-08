989297825a10dfb5ac7e46b88a571e9631553930
1pedrog <pedro.galeno.loiola@gmail.com>
2022-08-08 19:50:44 -0300

    Corrigi funcao validarSenha

--- senha-forte/validar.js
+++ senha-forte/validar.js
@@ -1,17 +1,31 @@
 const SENHA = document.getElementById('senha');
 const P_SENHA = document.getElementById('senha-tamanho');
+const DIV_PROGRESSO = document.getElementById('senha-progresso');
 
 function validarSenha() {
     if (SENHA.value.length < 8 ) {
        P_SENHA.innerHTML = 'Senha inválida!';
+       DIV_PROGRESSO.style.width = '5%';
+       DIV_PROGRESSO.style.backgroundColor = 'red';
+       return;
     }
-    if (SENHA.value.length = 8 ) {
-        P_SENHA.innerHTML = 'Senha fraca!';   
+
+    if (SENHA.value.length >= 8 ) {
+        P_SENHA.innerHTML = 'Senha fraca!';
+        DIV_PROGRESSO.style.width = '30%';
+        DIV_PROGRESSO.style.backgroundColor = 'red'; 
     }
-    if (true === isNaN(SENHA.value.length > 8 && SENHA.value.length < 12)) {
+
+    if (true === isNaN(SENHA.value)) {
         P_SENHA.innerHTML = 'Senha média!';
+        DIV_PROGRESSO.style.width = '70%';
+        DIV_PROGRESSO.style.backgroundColor = 'yellow';
     }
-    if (SENHA.value.length){
-        P_SENHA.innerHTML = 'Senha forte!!!';
+
+    let regex = /\W/; //Pegar os nao alfanumericos
+    if (true === regex.test(SENHA.value)) {
+        P_SENHA.innerHTML = 'Senha forte!';
+        DIV_PROGRESSO.style.width = '100%';
+        DIV_PROGRESSO.style.backgroundColor = 'green';
     }
 }
